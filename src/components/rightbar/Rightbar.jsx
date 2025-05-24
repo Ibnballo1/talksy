099d628d310bd8a9c5ca8edcf84ecb90bbdf3753
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-05-24 17:43:07 +0100

    Add right bar template

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,90 @@
 import './rightbar.css';
 
 export default function Rightbar() {
-  return <div className='rightbar'>Rightbar</div>;
+  return (
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img src='/gift.png' alt='' className='birthdayImg' />
+          <span className='birthdayText'>
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img src='/ad.png' alt='' className='rightbarAd' />
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className='rightbarfriendList'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/person/3.jpeg' alt='' className='rightbarProfileImg' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Musa Jiji</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
