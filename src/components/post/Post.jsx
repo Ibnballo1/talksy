5e7cba0532b7a1b4a699c90ca21e88847dfc6632
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-05-25 09:32:32 +0100

    add dummy data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,16 @@
 import { MoreVert } from '@mui/icons-material';
 import './post.css';
 import { Users } from '../../dummyData';
+import { useState } from 'react';
 
 export default function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className='post'>
       <div className='postWrapper'>
@@ -28,9 +36,19 @@ export default function Post({ post }) {
         </div>
         <div className='postBottom'>
           <div className='postBottomLeft'>
-            <img src='/like.png' alt='' className='likeIcon' />
-            <img src='/heart.png' alt='' className='likeIcon' />
-            <span className='postLikeCounter'>{post.like} people liked it</span>
+            <img
+              src='/like.png'
+              alt=''
+              className='likeIcon'
+              onClick={handleLike}
+            />
+            <img
+              src='/heart.png'
+              alt=''
+              className='likeIcon'
+              onClick={handleLike}
+            />
+            <span className='postLikeCounter'>{like} people liked it</span>
           </div>
           <div className='postBottomRight'>
             <span className='postCommentText'>{post.comment} Comments</span>
