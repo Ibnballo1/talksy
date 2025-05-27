fe22b8c2e6260d68109020e469d07ac0c6f8f9e0
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-05-27 15:18:47 +0100

    add profile page

---
+++ src/pages/home/profile/Profile.jsx
@@ -0,0 +1,40 @@
+import './profile.css';
+import Feed from '../../../components/feed/Feed';
+import Leftbar from '../../../components/leftbar/Leftbar';
+import Rightbar from '../../../components/rightbar/Rightbar';
+import TopBar from '../../../components/topBar/TopBar';
+
+export default function Profile() {
+  return (
+    <>
+      <TopBar />
+      <div className='profile'>
+        <Leftbar />
+        <div className='profileRight'>
+          <div className='profileRightTop'>
+            <div className='profileCover'>
+              <img
+                className='profileCoverImg'
+                src='./post/3.jpeg'
+                alt='cover picture'
+              />
+              <img
+                className='profileUserImg'
+                src='./person/7.jpeg'
+                alt='cover picture'
+              />
+            </div>
+            <div className='profileInfo'>
+              <h4 className='profileInfoName'>Raslaan Bello</h4>
+              <span className='profileInfoDesc'>Hello my friends</span>
+            </div>
+          </div>
+          <div className='profileRightBottom'>
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
